cc09634bf4a5aa7da5a0b526844cbfe376d499bb
Rezza016 <55936896+Rezza016@users.noreply.github.com>
2019-10-23 21:56:12 +0700

    Tugas2

---
+++ pgame-16099/code_tugas2.py
@@ -0,0 +1,7 @@
+# Ini Tugas 2. Praktik Git untuk MK Pemrograman Game
+
+print ("Hello, my name is Rezza") #Halo, nama saya Rezza
+print ("I enjoyed studying at your ummu") #Saya senang kuliah di ummu
+#baris ini tidak akan dieksekusi
+print ("Halo Anak Informatika...") #baris ini dieksekusi
+#Baris ini juga merupakan Baris Komentar 
